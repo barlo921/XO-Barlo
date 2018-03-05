96d6b5eb6fe919b8b474f549d3b5d06e6575a60a
grigoriikutuzov <barlo@list.ru>
2018-03-05 22:33:12 +0300

    Game and GameBuilder - bug fix

--- src/com/barlo/xo/model/Game.java
+++ src/com/barlo/xo/model/Game.java
@@ -3,23 +3,23 @@ package com.barlo.xo.model;
 
 public class Game {
 
-    private final String player1;
-    private final String player2;
+    private final Player player1;
+    private final Player player2;
 
     private final Field field;
 
-    Game(String player1, String player2, final int fieldSize) {
+    Game(Player player1, Player player2, final int fieldSize) {
         this.player1 = player1;
         this.player2 = player2;
 
         field = new Field(fieldSize);
     }
 
-    public String getPlayer1() {
+    public Player getPlayer1() {
         return player1;
     }
 
-    public String getPlayer2() {
+    public Player getPlayer2() {
         return player2;
     }
 
--- src/com/barlo/xo/model/GameBuilder.java
+++ src/com/barlo/xo/model/GameBuilder.java
@@ -1,16 +1,16 @@
 package com.barlo.xo.model;
 
 public class GameBuilder {
-    private String player1;
-    private String player2;
+    private Player player1;
+    private Player player2;
     private int fieldSize;
 
-    public GameBuilder setPlayer1(String player1) {
+    public GameBuilder setPlayer1(Player player1) {
         this.player1 = player1;
         return this;
     }
 
-    public GameBuilder setPlayer2(String player2) {
+    public GameBuilder setPlayer2(Player player2) {
         this.player2 = player2;
         return this;
     }
